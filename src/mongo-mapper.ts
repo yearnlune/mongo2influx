5fccc04e2446b638d7ae952bc1c0685839369396
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-03-18 13:02:42 +0900

    Add schema & model to mongo-mapper

--- src/mongo-mapper.ts
+++ src/mongo-mapper.ts
@@ -1,24 +1,27 @@
 import {DB_CONFIG} from './config';
-import mongoose from "mongoose";
+import mongoose, {Model, Mongoose, Schema, SchemaDefinition} from "mongoose";
 
 let INSTANCE: MongoMapper;
 
 export class MongoMapper {
-    private config: DB_CONFIG;
+    private _mongoose: Mongoose = mongoose;
+    private _config: DB_CONFIG;
     private _connection: mongoose.Connection | null = null;
+    private _schema: mongoose.Schema<any> | null = null;
+    private _model: mongoose.Model<any> | null = null;
 
     constructor(config: DB_CONFIG) {
-        this.config = config;
+        this._config = config;
         this.connect();
-        this._connection = mongoose.connection;
+        this._connection = this._mongoose.connection;
     }
 
     private makeUrl(): string {
-        return 'mongodb://' + this.config.host + ":" + this.config.port;
+        return 'mongodb://' + this._config.host + ":" + this._config.port + "/" + this._config.database;
     }
 
     private connect() {
-        return mongoose.connect(
+        return this._mongoose.connect(
             this.makeUrl(),
             {
                 useNewUrlParser: true,
@@ -30,8 +33,21 @@ export class MongoMapper {
         });
     }
 
+    setModel(collection: string, schema: Schema<any>) {
+        this._schema = schema;
+        this._model = this._mongoose.model(collection, schema);
+    }
+
     get connection(): any {
         return this._connection;
+    }
+
+    get schema(): mongoose.Schema<any> | null {
+        return this._schema;
+    }
+
+    get model(): mongoose.Model<any> | null {
+        return this._model;
     }
 }
 
@@ -42,4 +58,17 @@ export function mongoMapper(config: DB_CONFIG): MongoMapper {
 
 export function getConnection(): mongoose.Mongoose {
     return INSTANCE.connection;
+}
+
+export function setModel(schema: SchemaDefinition, collection: string) {
+    INSTANCE.setModel(collection, new mongoose.Schema(schema));
+    // return INSTANCE.connection.schema( new mongoose.Schema(schema));
+}
+
+export function getModel(): mongoose.Model<any> | null {
+    return INSTANCE.model;
+}
+
+export function getSchema(): mongoose.Schema<any> | null {
+    return INSTANCE.schema;
 }
